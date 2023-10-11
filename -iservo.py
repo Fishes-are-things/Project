f7341510f8dcb93cc7e986b4f11609403d729b14
Fishes-are-things <146807213+Fishes-are-things@users.noreply.github.com>
2023-10-11 18:10:42 +0800

    -iservo.py

---
+++ -iservo.py
@@ -0,0 +1,149 @@
+# Import libraries
+
+#Numbers 2 = 0°, 12 = 180°
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50) # pin 11 for servo1
+
+print("Wave maker starting")
+ # Start PWM running on both servos, value of 0 (pulse off)
+servo1.start(0)
+servo2.start(0)
+
+import keyboard
+
+while True:
+  
+
+    do_something()
+  time.sleep(0.5)
+  servo1.ChangeDutyCycle(2)
+
+  # Wait for 2 seconds
+  time.sleep(2)
+
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(2)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(0.5)
+
+  servo1.ChangeDutyCycle(12)
+  time.sleep(0.5)
+  servo1.ChangeDutyCycle(2)
+
+  # Wait again for 2 seconds! :)
+  time.sleep(2)
+
+  #reset
+
+
+  servo1.ChangeDutyCycle(7)
+  time.sleep(2)
+ 
+  servo1.ChangeDutyCycle(7)
+  
+
+
+  time.sleep(5)
+
+###############################################
+
+  servo1.ChangeDutyCycle(7)
+  time.sleep(2)
+ 
+  servo1.ChangeDutyCycle(7)
+  
+
+#Clean things up at the end
+servo1.stop()
+servo2.stop()
+GPIO.cleanup()
+print ("wave maker finished")
